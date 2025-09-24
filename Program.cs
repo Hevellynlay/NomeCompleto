39dba6f5aa4d3abd921c5d2e506576808845bf0a
Hevellyn <celularhduarte@gmail.com>
2025-09-24 20:13:45 -0300

    Exercício Finalizado

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Digite seu nome: ");
+string nome = Console.ReadLine()!;
+
+Console.Write("Digite seu sobrenome: ");
+string sobrenome = Console.ReadLine()!;
+
+Console.Write($"Boa noite, {nome} {sobrenome}, tudo bem?");
